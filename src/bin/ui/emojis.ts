8f222945a7a6aaf4fdccc55f2a01cd3c93f87d99
Olga Shevchenko <olga.n.sh@gmail.com>
2022-01-12 15:44:02 +0000

    NB-48: Installation fixes

--- src/bin/ui/emojis.ts
+++ src/bin/ui/emojis.ts
@@ -4,6 +4,7 @@ export const EMOJIS = {
   ZAP: get('zap'),
   COFFEE: get('coffee'),
   SCREAM: get('scream_cat'),
+  SUNGLASSES: get('sunglasses'),
   ROCKET: get('rocket'),
   POINT_RIGHT: get('point_right'),
   MAILBOX_WITH_EMAIL: get('mailbox_with_mail'),

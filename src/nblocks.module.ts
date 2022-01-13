4d7bfec2ae126b30a295cabc4d255ab7504687e9
Olga Shevchenko <olga.n.sh@gmail.com>
2022-01-13 16:20:37 +0000

    NB-52: copy shared services to Nblocks module

--- src/nblocks.module.ts
+++ src/nblocks.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TenantModule } from './tenant/tenant.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
 import { FileModule } from './file/file.module';
 import { NebulrConfigModule } from './nebulr/nebulr-config/nebulr-config.module';
 import { AuthModule } from './nebulr-auth/nebulr-auth.module';
+import { ClientService } from './shared/client/client.service';
+import { CacheService } from './shared/cache/cache.service';
 
 @Module({
   imports: [
@@ -13,9 +15,11 @@ import { AuthModule } from './nebulr-auth/nebulr-auth.module';
     UserModule,
     TenantModule,
     FileModule,
-    WebhooksModule
+    WebhooksModule,
+    CacheModule.register(),
   ],
   controllers: [],
-  providers: [],
+  providers: [CacheService, ClientService],
+  exports: [CacheService, ClientService],
 })
 export class NBlocksModule { }

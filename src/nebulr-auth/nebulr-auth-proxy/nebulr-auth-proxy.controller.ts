3282b6720cec4b1c44e375f57008c4e8b15d427f
Oscar Söderlund <oscar@nebulr.group>
2022-03-22 19:57:40 +0100

    Updates, locking ts-lib version

--- src/nebulr-auth/nebulr-auth-proxy/nebulr-auth-proxy.controller.ts
+++ src/nebulr-auth/nebulr-auth-proxy/nebulr-auth-proxy.controller.ts
@@ -1,4 +1,10 @@
 import { AuthenticateRequestDto, AuthenticateResponseDto, AuthenticatedResponse, UpdatePasswordRequestDto, UpdateUserInfoRequestDto, AuthTenantUserResponseDto } from '@nebulr-group/nblocks-ts-client';
+import { CommitMfaCodeRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/commit-mfa-code-request.dto';
+import { CommitMfaCodeResponseDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/commit-mfa-code-response.dto';
+import { FinishUserMfaSetupRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/finish-user-mfa-setup-request.dto';
+import { FinishUserMfaSetupResponseDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/finish-user-mfa-setup-response.dto';
+import { ResetUserMfaSetupRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/reset-user-mfa-setup-request-dto';
+import { StartUserMfaSetupRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/start-user-mfa-setup-request-dto';
 import { Body, Controller, Get, Post, Put, Headers } from '@nestjs/common';
 import { ForgotPasswordRequestDto } from './dto/forgot-password-request.dto';
 import { NebulrAuthProxyService } from './nebulr-auth-proxy.service';
@@ -13,6 +19,26 @@ export class NebulrAuthProxyController {
     @Headers('user-agent') userAgent: string,
   ): Promise<AuthenticateResponseDto> {
     return this.proxyService.authenticate(request, userAgent);
+  }
+
+  @Post('commitMfaCode')
+  async commitMfaCode(@Body() request: CommitMfaCodeRequestDto): Promise<CommitMfaCodeResponseDto> {
+    return this.proxyService.commitMfaCode(request);
+  }
+
+  @Post('startMfaUserSetup')
+  async startMfaUserSetup(@Body() request: StartUserMfaSetupRequestDto): Promise<void> {
+    await this.proxyService.startMfaUserSetup(request);
+  }
+
+  @Post('finishMfaUserSetup')
+  async finishMfaUserSetup(@Body() request: FinishUserMfaSetupRequestDto): Promise<FinishUserMfaSetupResponseDto> {
+    return this.proxyService.finishMfaUserSetup(request);
+  }
+
+  @Post('resetUserMfaSetup')
+  async resetUserMfaSetup(@Body() request: ResetUserMfaSetupRequestDto): Promise<void> {
+    return this.proxyService.resetUserMfaSetup(request);
   }
 
   @Get('authenticated')
--- src/nebulr-auth/nebulr-auth-proxy/nebulr-auth-proxy.service.ts
+++ src/nebulr-auth/nebulr-auth-proxy/nebulr-auth-proxy.service.ts
@@ -1,4 +1,10 @@
 import { AuthenticateRequestDto, AuthenticateResponseDto, AuthenticatedResponse, DeauthenticateResponse, UpdatePasswordRequestDto, UpdateUserInfoRequestDto, AuthTenantUserResponseDto } from '@nebulr-group/nblocks-ts-client';
+import { CommitMfaCodeRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/commit-mfa-code-request.dto';
+import { CommitMfaCodeResponseDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/commit-mfa-code-response.dto';
+import { FinishUserMfaSetupRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/finish-user-mfa-setup-request.dto';
+import { FinishUserMfaSetupResponseDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/finish-user-mfa-setup-response.dto';
+import { ResetUserMfaSetupRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/reset-user-mfa-setup-request-dto';
+import { StartUserMfaSetupRequestDto } from '@nebulr-group/nblocks-ts-client/dist/platform/auth/models/start-user-mfa-setup-request-dto';
 import { Injectable } from '@nestjs/common';
 import { ClientService } from '../../shared/client/client.service';
 
@@ -15,6 +21,40 @@ export class NebulrAuthProxyService {
     userAgent: string
   ): Promise<AuthenticateResponseDto> {
     return this.clientService.client.auth.authenticate(request, userAgent);
+  }
+
+  /**
+     * Commit the MFA OTP code recieved
+     * @param args `CommitMfaCodeRequestDto`
+     * @returns `CommitMfaCodeResponseDto` including MFA token to be used in future calls
+     */
+  async commitMfaCode(args: CommitMfaCodeRequestDto): Promise<CommitMfaCodeResponseDto> {
+    return this.clientService.client.auth.commitMfaCode(args);
+  }
+
+  /**
+   * Initiates the MFA setup process for users missing phone number
+   * @param args `StartUserMfaSetupRequestDto`
+   */
+  async startMfaUserSetup(args: StartUserMfaSetupRequestDto): Promise<void> {
+    await this.clientService.client.auth.startMfaUserSetup(args);
+  }
+
+  /**
+   * Finish the MFA setup process by providing the received OTP thus validating the phone numer and recieve MFA token and backup code
+   * @param args `FinishUserMfaSetupRequestDto`
+   * @returns `FinishUserMfaSetupResponseDto` including MFA token to be used in future calls and the backup code that can be used to reset the MFA in the future. The backup code should be stored safely
+   */
+  async finishMfaUserSetup(args: FinishUserMfaSetupRequestDto): Promise<FinishUserMfaSetupResponseDto> {
+    return this.clientService.client.auth.finishMfaUserSetup(args);
+  }
+
+  /**
+   * Use the previously recieved backup code to reset the MFA setup. This allows `startMfaUserSetup` to be called again
+   * @param args `ResetUserMfaSetupRequestDto`
+   */
+  async resetUserMfaSetup(args: ResetUserMfaSetupRequestDto): Promise<void> {
+    return this.clientService.client.auth.resetUserMfaSetup(args);
   }
 
   public async authenticated(

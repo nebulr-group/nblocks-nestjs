ff0fc5f50e5790f77d53fdefb44636f947d5f5ef
Nemanja Manojlovic <nemanja@nebulr.group>
2022-09-14 00:12:11 +0200

    Create publish.sh script for creating .npmrc

--- init.sh
+++ init.sh
@@ -2,10 +2,4 @@
 
 ## This file setups all prerequisites for the docker container, used both dev environment and in CI/CD pipeline.
 ## For prerequisites strictly related to dev environment see ./.devcontainer/init.sh which is run before this script.
-
-npm config set @nebulrgroup:registry https://gitlab.com/api/v4/projects/23533518/packages/npm/
-npm config set '//gitlab.com/api/v4/projects/23533518/packages/npm/:_authToken' $GITLAB_NPM_REPOSITORY_TOKEN
-
-npm install
-
 echo "Done initializing"

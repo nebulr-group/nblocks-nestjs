fda80d01033ec8ff13df606a66ba2074b21080db
Nemanja Manojlovic <nemanja@nebulr.group>
2022-09-14 15:42:38 +0200

    Add install for less, curl and jq to init.sh, hot-fix secrets.env typo

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,13 +2,13 @@
 ## Usage
 ## ./init_local.sh WORKSPACE_FOLDER_PATH
 PWD=$(dirname $0)
-source $PWD/secrets.env.default
+source $PWD/secrets.env
 
 
-echo "Installing vim"
+echo "Updating and installing tools"
 apt-get update
-apt-get -y install vim
-# microsoft docker container contains no vim but vim.tiny
+apt-get -y install vim less curl jq
+# Git tool config
 echo "Configuring GIT"
 git config --global core.editor "vim"
 git config --global user.email $GIT_EMAIL
